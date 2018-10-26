6b01575a5a05cb3afb9561eac67f3e34e014c616
Neil Ghosh <sabyasachi.ghosh@jda.com>
2018-10-26 14:49:58 +0530

    Add Text to Speech integration

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const debug = require('@google-cloud/debug-agent').start({ allowExpressions: true });
 
 exports.helloWorld = (req, res) => {
-    res.send('Hello World!');
+
+  const text = req.query.text || 'No text provided.';
+  const lang = req.query.lang || 'en-US';
+  const gender = req.query.gender || 'NEUTRAL';
+  const textToSpeech = require('@google-cloud/text-to-speech');
+
+  // Creates a client
+  const client = new textToSpeech.TextToSpeechClient();
+
+  // Construct the request
+  const request = {
+    input: { text: text },
+    // Select the language and SSML Voice Gender (optional)
+    voice: { languageCode: lang, ssmlGender: gender },
+    // Select the type of audio encoding
+    audioConfig: { audioEncoding: 'MP3' },
+  };
+
+  // Performs the Text-to-Speech request
+  client.synthesizeSpeech(request, (err, response) => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+    res.set('Content-Type', 'Content-Type: audio/mpeg');
+    res.set('Content-Disposition', 'attachment; filename="output.mp3');
+    res.send(new Buffer(response.audioContent, 'binary'))
+  });
 };
